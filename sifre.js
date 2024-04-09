6ef8e6bef98175005ffa49af02ef47b085eb5dfa
sevvalbayrm <102861519+sevvalbayrm@users.noreply.github.com>
2024-04-09 22:04:08 +0300

    Update sifre.js

--- sifre.js
+++ sifre.js
@@ -8,9 +8,7 @@ function checkPasswords(event) {
     if (password === confirmPassword) {
       document.getElementById('message').innerText = 'Kayıt Başarılı';
       document.getElementById('message').style.color = 'green';
-      setTimeout(function() {
-        window.location.href = 'giris.html';
-      }, 3000);
+      
     } else {
       document.getElementById('message').innerText = 'Şifreler eşleşmiyor, lütfen tekrar deneyin.';
       document.getElementById('message').style.color = 'red';
@@ -24,10 +22,5 @@ function checkPasswords(event) {
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
     var errorMessageElement = document.getElementById('errorMessage');
-  //veritabanında olunca buraya gerek var mı sor.
-    if (email === "sevval@gmail.com" && password === "123456") {
-        
-        console.log("Giriş başarılı");
-      } 
        
   }
